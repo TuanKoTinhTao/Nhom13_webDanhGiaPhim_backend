762fa46a8759faea4671be54002b9214149f12c2
PTriet <nguyentriet0112@gmail.com>
2024-11-14 14:48:33 +0700

    resolve CORS

--- src/main/java/com/film/movies/ReviewController.java
+++ src/main/java/com/film/movies/ReviewController.java
@@ -6,13 +6,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Repository;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
-
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
